5e2818e6f5ad1db967e33c4ac3d514362206e1b5
MBlaser360 <73309005+MBlaser360@users.noreply.github.com>
2022-04-04 23:31:26 -0500

    p

--- index.js
+++ index.js
@@ -31,4 +31,24 @@ express()
   }
 })
 
+.get('/db-info', async(req, res) => {
+	try {
+		const client = await pool.connect();
+		const tabels = await client.query (
+			"SELECT c.relname AS table, a.attname AS column, t.typename AS type FROM pg.catalog.pg_class AS c LEFT JOIN pg.catalog.pg_attribute AS a ON c.oid = a.attreild AND a.attnum > 0 LEFT JOIN pg.catalog.pg_type AS t ON a.atttypid = t.oid WHERE c.relname IN ('users', 'observations', 'students', 'schools, 'tasks') ORDER BY crelname, a.attnum;"
+
+
+		);
+
+		const locals = {
+			'tabels': (tabels) ? tabels.rows : null
+		};
+
+		res.render('pages/db-info', locals);
+		client.release();
+	} catch {
+		console.error(err);
+		res.send("Error: " + err)
+	}
+})
 .listen(PORT, () => console.log('listening on ${ PORT }'));
