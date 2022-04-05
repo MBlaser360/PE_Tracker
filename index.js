40929b6cf38660b53778e01e46bdd9a28b1385c9
MBlaser360 <73309005+MBlaser360@users.noreply.github.com>
2022-04-04 22:51:32 -0500

    test

--- index.js
+++ index.js
@@ -12,15 +12,23 @@ const pool = new Pool({
   }
 });
 
-express().use(express.static(path.join(__dirname, 'public'))).use(express.json()).use(express.urlencoded({ extended: true})).set('views', path.join(__dirname, 'views')).set('view engine', 'ejs').get('/', async(req, res) => {
+express()
+	.use(express.static(path.join(__dirname, 'public')))
+	.use(express.json())
+	.use(express.urlencoded({ extended: true}))
+	.set('views', path.join(__dirname, 'views'))
+	.set('view engine', 'ejs')
+	.get('/', async(req, res) => {
   
   try {
     const client = await pool.connect();
 
     client.release();
-		
+
   } catch (err) {
     console.error(err);
     res.send("Error" + err);
   }
 })
+
+.listen(PORT, () => console.log('listening on ${ PORT }'));
