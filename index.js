35a3a7fcc55aa74a12d6979ba7098ac1004af945
MBlaser360 <73309005+MBlaser360@users.noreply.github.com>
2022-04-04 22:37:30 -0500

    Test response for / route.

--- index.js
+++ index.js
@@ -7,4 +7,24 @@ const pool = new Pool ({
 	connectionString: process.env.DATABASE_URL, ssl: {
 		rejectUnauthorized: false
 	}
-})
+});
+
+express()
+  .use(exress.static(path.join(__dirname, 'public')))
+  .use(express.json()) 
+	.use(express.urlencoded({ extended: true}))
+	.set('views', path.join(__dirname, 'views'))
+	.set('view engine', 'ejs')
+	.get('/', async(req, res) => {
+			try {
+				const client = await pool.connect();
+
+				client.release();
+			}
+			catch (err) {
+				console.error(err);
+				res.send("Error " + err);
+			}
+	})
+
+	.listen(PORT, () => console.log('Listening on ${ PORT }'));
