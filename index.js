817739bfaf3a86a2ad8114154c2a7c1c232f4798
MBlaser360 <73309005+MBlaser360@users.noreply.github.com>
2022-04-04 22:45:46 -0500

    hopefully it works

--- index.js
+++ index.js
@@ -1,30 +1,26 @@
 const express = require('express');
+const req = require('express/lib/request');
 const path = require('path');
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5432;
 const { Pool } = require('pg');
 
-const pool = new Pool ({
-	connectionString: process.env.DATABASE_URL, ssl: {
-		rejectUnauthorized: false
-	}
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
-express()
-  .use(exress.static(path.join(__dirname, 'public')))
-  .use(express.json()) 
-	.use(express.urlencoded({ extended: true}))
-	.set('views', path.join(__dirname, 'views'))
-	.set('view engine', 'ejs')
-	.get('/', async(req, res) => {
-			try {
-				const client = await pool.connect();
+express().use(express.static(path.join(__dirname, 'public'))).use(express.json()).use(express.urlencoded({ extended: true})).set('views', path.join(__dirname, 'views')).set('view engine', 'ejs').get('/', async(req, res) => {
+  
+  try {
+    const client = await pool.connect();
 
-				client.release();
-			}
-			catch (err) {
-				console.error(err);
-				res.send("Error " + err);
-			}
-	})
-
-	.listen(PORT, () => console.log('Listening on ${ PORT }'));
+    client.release();
+		
+  } catch (err) {
+    console.error(err);
+    res.send("Error" + err);
+  }
+})
